426b80a49cbf0e31abaee7102ae395e458957293
Mingaleev-D92 <mingaleev,d92@mail.ru>
2022-12-17 18:35:25 +0300

    показ описания элумента карточки айтома

--- app/src/main/java/com/example/fakedemo/ui/epoxyController/UiProductEpoxyController.kt
+++ app/src/main/java/com/example/fakedemo/ui/epoxyController/UiProductEpoxyController.kt
@@ -20,7 +20,8 @@ class UiProductEpoxyController(
             val epoxyId = it + 1
             UiProductEpoxyModel(
                uiProduct = null,
-               onFavoriteIconClicked = ::onFavoriteIconClicked
+               onFavoriteIconClicked = ::onFavoriteIconClicked,
+               onUiProductClicked = ::onUiProductClicked
             ).id(epoxyId).addTo(this)
          }
          return
@@ -28,7 +29,8 @@ class UiProductEpoxyController(
       data.forEach { uiProduct ->
          UiProductEpoxyModel(
             uiProduct = uiProduct,
-            onFavoriteIconClicked = ::onFavoriteIconClicked
+            onFavoriteIconClicked = ::onFavoriteIconClicked,
+            onUiProductClicked = ::onUiProductClicked
          ).id(uiProduct.product.id).addTo(this)
       }
    }
@@ -43,6 +45,19 @@ class UiProductEpoxyController(
                currentFavoriteIds + setOf(selectedProductId)
             }
             return@update currentState.copy(favoriteProductIds = newFavoriteIds)
+         }
+      }
+   }
+   private fun onUiProductClicked(productId: Int) {
+      viewModel.viewModelScope.launch {
+         viewModel.store.update { currentState ->
+            val currentExpandedIds = currentState.expandedProductIds
+            val newExpandedIds = if (currentExpandedIds.contains(productId)) {
+               currentExpandedIds.filter { it != productId }.toSet()
+            } else {
+               currentExpandedIds + setOf(productId)
+            }
+            return@update currentState.copy(expandedProductIds = newExpandedIds)
          }
       }
    }
